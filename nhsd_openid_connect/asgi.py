ac5447f862a7684e0bf69c2d5cc50dbb11029bbe
Becky Smith <becky.smith@thedatalab.org>
2021-04-26 12:43:42 +0100

    Black changes missed in initial commit

--- nhsd_openid_connect/asgi.py
+++ nhsd_openid_connect/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nhsd_openid_connect.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nhsd_openid_connect.settings")
 
 application = get_asgi_application()
