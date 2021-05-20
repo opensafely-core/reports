ee289669f4dcaeda30e77517f68564d4a36a51ab
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2021-05-20 17:02:20 +0100

    Initial styles for output page

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,32 @@ module.exports = {
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.gray.800'),
+            'h1, h2, h3, h4, h5, h6': {
+              color: theme('colors.gray.800'),
+            },
+            'ul > li::before': {
+              'background-color': theme('colors.gray.800')
+            },
+            'pre': {
+              'background-color': theme('colors.gray.50'),
+              'color': theme('colors.gray.700')
+            },
+            'thead th': {
+              'text-align': 'left'
+            },
+            'tbody tr:nth-child(even)': {
+              'background-color': theme('colors.gray.50'),
+            },
+            'tbody tr:hover': {
+              'background-color': theme('colors.blue.50'),
+            }
+          },
+        },
+      }),
     },
   },
   variants: {
