72189efda58d4b4c17ab8baaab88f4999e1fedb7
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2021-05-25 08:57:07 +0100

    Remove table striping and hover effects

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,15 +32,6 @@ module.exports = {
             'thead th': {
               'text-align': 'left'
             },
-            'tbody tr:nth-child(even), tbody tr:nth-child(even) th:not([rowspan])': {
-              'background-color': theme('colors.gray.50'),
-            },
-            'tbody th[rowspan], tbody tr:hover th[rowspan]': {
-              'background-color': theme('colors.white'),
-            },
-            'tbody tr:hover, tbody tr:hover th:not([rowspan])': {
-              'background-color': theme('colors.blue.50'),
-            }
           },
         },
       }),
