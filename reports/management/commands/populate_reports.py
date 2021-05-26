e15d37fb831971ee1549adc91b5bf139fcf9f624
Ben Butler-Cole <ben@bridesmere.com>
2021-05-26 10:55:29 +0100

    Trigger validation when populating dev database

--- reports/management/commands/populate_reports.py
+++ reports/management/commands/populate_reports.py
@@ -10,14 +10,37 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         category, _ = Category.objects.get_or_create(name="Reports")
-        report, created = Report.objects.get_or_create(
-            category=category,
-            title="Vaccine Coverage",
-            repo="nhs-covid-vaccination-coverage",
-            branch="master",
-            report_html_file_path="released-reports/opensafely_vaccine_report_overall.html",
-            publication_date=datetime.datetime(year=2021, month=5, day=10),
+
+        self.ensure_report(
+            category,
+            "Vaccine Coverage",
+            "output-explorer-test-repo",
+            "master",
+            "test-outputs/vaccine-coverage-new.html",
         )
 
+    def ensure_report(self, category, title, repo, branch, file_path):
+        report, created = get_or_create_with_validation(
+            Report,
+            category=category,
+            title=title,
+            repo=repo,
+            branch=branch,
+            report_html_file_path=file_path,
+            publication_date=datetime.datetime(year=2021, month=5, day=10),
+        )
         if created:
             self.stderr.write(f"Created report '{report.title}'")
+
+
+# The built-in get_or_create() doesn't do validation, which we want here for safety and because it triggers the setup
+# of derived fields as a side-effect. We follow the outline of the built-in version, but add a call to do the
+# validation.
+def get_or_create_with_validation(model, **params):
+    try:
+        return model.objects.get(**params), False
+    except model.DoesNotExist:
+        o = model(**params)
+        o.full_clean()
+        o.save()
+        return o, True
