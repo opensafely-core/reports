ec7ad55276c8dcbdcc072874b49ba4605a027c45
George Hickman <george@ghickman.co.uk>
2022-07-20 13:21:34 +0100

    Consolidate templates into a single directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: [
     "./templates/**/*.html",
-    "./reports/templates/**/*.html",
   ],
   theme: {
     extend: {
