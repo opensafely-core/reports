51f37ba87973270abed15b3a243025dfb093ac92
George Hickman <george@ghickman.co.uk>
2022-07-26 08:10:12 +0100

    default all sources in CSP to 'none'

--- reports/settings.py
+++ reports/settings.py
@@ -181,6 +181,7 @@ CACHES = {
 # CSP
 # https://django-csp.readthedocs.io/en/latest/configuration.html
 CSP_REPORT_ONLY = DEBUG
+CSP_DEFAULT_SRC = ["'none'"]
 CSP_STYLE_SRC_ELEM = ["'self'", "https://fonts.googleapis.com"]
 CSP_SCRIPT_SRC_ELEM = ["'self'"]
 CSP_FONT_SRC = ["'self'", "https://fonts.gstatic.com"]
