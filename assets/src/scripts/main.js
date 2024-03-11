fa8e392cc89291925a6d282a057ba47bcbe46153
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2024-03-11 14:43:34 +0000

    Remove Alpine.js and associated dependencies

--- assets/src/scripts/main.js
+++ assets/src/scripts/main.js
@@ -1,13 +1,3 @@
-import Alpine from "alpinejs/packages/csp/dist/module.esm";
-import Screen from "@alpine-collective/toolkit-screen/dist/module.esm";
 import "./_ie11";
 import "./_plausible";
 import "../styles/main.scss";
-import userMenu from "./alpine/user-menu";
-
-Alpine.plugin(Screen);
-
-Alpine.data("userMenu", userMenu);
-
-window.Alpine = Alpine;
-window.Alpine.start();
