8c167bb3316010bb3804ba2318a62b1bd1cb9051
Steven Maude <git@stevenmaude.co.uk>
2026-08-05 09:22:56 +0000

    Make datetimes timezone aware
    Fix the following:
    
    * "DTZ001 `datetime.datetime()` called without a `tzinfo` argument"
    * "DTZ005 `datetime.datetime.now()` called without a `tz` argument"
    * "DTZ007 Naive datetime constructed using
      `datetime.datetime.strptime()` without %z"
    
    Note that:
    
    * `publication_date` only needed to be a value for a
      `DateField`, so we can replace the `datetime` with a `date`.
    * HTTP-date values are in UTC, although indicated by the abbreviation
      UTC.
    
    Co-authored by: copilot-swe-agent[bot] <198982749+Copilot@users.noreply.github.com>

--- reports/management/commands/populate_reports.py
+++ reports/management/commands/populate_reports.py
@@ -58,7 +58,11 @@ class Command(BaseCommand):
             Report,
             category=category,
             org=org,
-            publication_date=datetime.datetime(year=2021, month=5, day=10),
+            publication_date=datetime.date(
+                year=2021,
+                month=5,
+                day=10,
+            ),
             created_by=user,
             updated_by=user,
             **kwargs,
